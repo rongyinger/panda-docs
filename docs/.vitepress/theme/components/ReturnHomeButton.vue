0c866a6050e3a61c02950aaa9cdb174dfe229c67
rongying <1733076900@qq.com>
2026-05-29 18:47:07 +0800

    Unify doc pages with homepage style

---
+++ docs/.vitepress/theme/components/ReturnHomeButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <a
+    v-if="!isHome"
+    class="return-home-button"
+    :href="withBase('/')"
+    aria-label="返回首页"
+  >
+    <span class="return-home-mark">⌂</span>
+    <span>返回首页</span>
+  </a>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute, withBase } from 'vitepress'
+
+const route = useRoute()
+const isHome = computed(() => route.path === '/' || route.path === '/index.html')
+</script>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@ import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
 import Feedback from './components/Feedback.vue'
 import ContactFloat from './components/ContactFloat.vue'
+import ReturnHomeButton from './components/ReturnHomeButton.vue'
 import HomePageSections from './components/HomePageSections.vue'
 import HomeSteps from './components/HomeSteps.vue'
 import HomeHotFaq from './components/HomeHotFaq.vue'
@@ -15,7 +16,7 @@ export default {
       'nav-bar-title-after': () =>
         h('span', { class: 'nav-subtitle' }, '熊猫算力官方知识库'),
       'doc-after': () => h(Feedback),
-      'layout-bottom': () => h(ContactFloat),
+      'layout-bottom': () => [h(ReturnHomeButton), h(ContactFloat)],
     })
   },
 
